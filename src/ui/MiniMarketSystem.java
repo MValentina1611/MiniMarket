10e96449d307dbbbfca975a5dc0330b2e4e4f3b7
M. Valentina Castro <valentinacastro1516@gmail.com>
2021-10-16 20:18:53 -0500

    Menu Ready

--- src/ui/MiniMarketSystem.java
+++ src/ui/MiniMarketSystem.java
@@ -1,8 +1,78 @@
 package ui;
 
+import java.io.IOException;
+import java.util.Scanner;
+
+import model.MiniMarket;
+
 public class MiniMarketSystem {
 
+	private Scanner reader;
+	private MiniMarket miniMarket;
 	
+	public MiniMarketSystem()
+	{
+		reader = new Scanner(System.in);
+		miniMarket = new MiniMarket();
+	}
 	
+	public static void main(String args[])
+	{
+		System.out.println("The App is initializing...");
+		
+		MiniMarketSystem mms = new MiniMarketSystem();  
+		
+		 int menuOp = 0;
+
+			do
+			{
+				menuOp = mms.showMenu();
+				mms.executeOptions(menuOp);
+
+			}while( menuOp != 0 );
+			
+		
+	}
+	
+	public int showMenu()
+	{
+		int menuOp = 0;
+
+		String menu = 
+			"===== WELCOME TO THE MINIMARKET MI BARRIO TE QUIERE =====\n" +
+			"Pick an option \n" +
+			"(1) Register a person\n" +
+			"(2) Number of people who have tried to enter\n"+								
+			"(0) Exit";
+		
+		System.out.println(menu);
+		menuOp = reader.nextInt();
+		reader.nextLine();
+		return menuOp;
+
+	}//Method ends
+	
+	public void executeOptions( int option )
+	{
+		switch( option ) 
+		{
+
+			case 0:
+				System.out.println ("BYE :*");
+				break;
+			case 1:
+				System.out.println("Register a person");
+				break;
+	
+			case 2:
+				System.out.println("Number of people");
+				break;
+		
+			default:
+				System.out.println("Error, invalid option");
+				
+		}//Switch ends
+		
+	}//Method ends
 	
 }
